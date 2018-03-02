a56b714f5183a94b215bd65d95608daae5e0cb44
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2018-03-03 00:03:35 +0600

    placeholder login service is working

---
+++ src/main/java/dreamteam/LoginResource.java
@@ -0,0 +1,23 @@
+package dreamteam;
+
+import javax.inject.Singleton;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Singleton
+@Path("login")
+public class LoginResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response getIt(String json) {
+        return Response.ok().build();
+    }
+}
